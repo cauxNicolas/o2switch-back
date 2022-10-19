a67658d34049c4f8032cf0bf12c6c485d846a8d1
cauxNicolas <nicaux95@gmail.com>
2022-10-19 21:15:07 +0200

    test mongo vercel

--- index.js
+++ index.js
@@ -9,25 +9,16 @@ app.use(formidableMiddleware());
 app.use(cors());
 
 //// MONGOOSE
-// mongoose.connect(process.env.MONGO);
+mongoose.connect(process.env.MONGO);
 
-/* const Text = mongoose.model("Text", {
+const Text = mongoose.model("Text", {
 	description: String,
 });
- */
+
 //// GET
 app.get("/", async (req, res) => {
 	try {
-		const textInput = {
-			message: [
-				{
-					_id: "63500bdef5dfabe8fed369ea",
-					description: "Test vercel ",
-					__v: 0,
-				},
-			],
-		};
-		// const textInput = await Text.find();
+		const textInput = await Text.find();
 		res.json({ message: textInput });
 	} catch (error) {
 		res.status(400).json({ error: error.message });
@@ -42,12 +33,12 @@ app.post("/post", async (req, res) => {
 	try {
 		//console.log("2");
 
-		/* const newInput = new Text({
+		const newInput = new Text({
 			description: req.fields.input,
-		}); */
+		});
 		//console.log("3");
 
-		//await newInput.save();
+		await newInput.save();
 		//console.log("4");
 
 		res.json({
@@ -58,7 +49,7 @@ app.post("/post", async (req, res) => {
 		//console.log("6");
 
 		res.status(400).json({ error: error.message });
-		console.log("7");
+		// console.log("7");
 	}
 	//console.log("8");
 });
